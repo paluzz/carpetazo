4d1bfff7b0371e4250ccfd5fc5b3680de0626a51
Damian <paluzdam@gmail.com>
2023-08-11 00:22:10 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# carpetazo
-Busqueda de tweets polemicos
+# Carpetazos en Twitter - README
+
+Este es un proyecto de una aplicación web simple que te permite buscar tweets polemicos del usuario que quieras en Twitter utilizando JavaScript puro (Vanilla JS).
+
+## Modo de Uso
+
+- **Selecciona el usuario:** Ingresa el usuario al que quieres encontrarle tweets polemicos en el campo de búsqueda.
+
+- **Selecciona el tipo de carpetazo a realizar:** Selecciona el tipo de carpetazo que deseas realizar y presiona el botón "Buscar" para obtener una lista de tweets.
+
+- **Visualización de Resultados:** Los resultados de la búsqueda se mostrarán en tiempo real a medida que se obtengan de la API de Twitter. Cada resultado incluirá el contenido del tweet, el nombre de usuario y la fecha de creación.
+
+## Requisitos
+
+- Navegador web moderno que admita JavaScript.
+
---
+++ script.js
@@ -0,0 +1,72 @@
+const searchWords = (...strings) => {
+    let resultado = '%20%28';
+    for (let i = 0; i < strings.length; i++) {
+        if (resultado !== '%20%28') {
+            resultado += '%20OR%20';
+        }
+        if (strings[i] == ' ') {
+            resultado += '%20';
+        }
+        else {
+            resultado += strings[i];
+        }
+    }
+    return resultado.concat('%29');
+}
+const resetError = () => {
+    setTimeout(() => {
+        optionError.innerHTML = '';
+    }, 2500);
+}
+const openWindow = (searchQuery) => {
+    window.open(searchQuery, '_blank')
+}
+function buscarTweets() {
+    const twitterUsername = document.getElementById('twitterUsername').value
+    const opcion = document.getElementById('filtros')
+    const optionError = document.getElementById('optionError');
+    const twitterSearch = "https://twitter.com/search?q=from%3A";
+    let searchQuery = twitterSearch + `${twitterUsername}`;
+
+    if (twitterUsername.length > 0) {
+        switch (opcion.value) {
+            case 'machistas':
+                searchQuery += searchWords('mujer', 'mujeres', 'putas');
+                openWindow(searchQuery);
+                break;
+            case 'homofob':
+                searchQuery += searchWords('gay', 'gays', 'putos', 'trolos');
+                openWindow(searchQuery);
+                break;
+            case 'racistas':
+                searchQuery += searchWords('negro', 'negros', 'marron', 'marrones', 'nword');
+                openWindow(searchQuery);
+                break;
+            case 'aborto':
+                searchQuery += searchWords('aborto', 'niunamenos', 'dos vidas', 'anti derechos');
+                openWindow(searchQuery);
+                break;
+            case 'messi':
+                searchQuery += `%20messi%20until%3A2021-01-01`
+                openWindow(searchQuery);
+                break;
+            case 'electoral':
+                searchQuery += searchWords('milei', 'massa', 'larreta', 'bullrich');
+                openWindow(searchQuery);
+                break;
+            case 'seleccione':
+                optionError.innerHTML = 'Ingrese una opción válida!!!';
+                resetError();
+                break;
+            default:
+                break;
+        }
+    } else {
+        optionError.innerHTML = 'Ingrese un usuario valido!!!';
+        resetError();
+    }
+}
+
+
+
+
